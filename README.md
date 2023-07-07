64028d58cb8b12b8ddace8f76c2a7c82f86ac0e5
Tekrodz <kevinkmmichel@gmail.com>
2023-07-08 00:11:08 +0200

    Add function search domaine & department + domain

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ For install
 npm install oss-api-td5
 ```
 
+## Requirements
+
+- Node.JS
+- npm
+
 ## Use functions
 
 I. Get festival by departments
@@ -23,3 +28,48 @@ getFestivalByDepartment(x)
 This function search in the API for the festival in the department you enter in the params
 
 x = department number (exemple : 01, 25 or 65)
+
+II. Get festival by domaine
+
+Use the function :
+
+```
+getFestivalByDomaine(x)
+```
+
+This function search in the API for the festival in the domain you enter in the params
+
+x = domaine number (refer to the table under)
+
+| Value | Number |
+| :----:|:------:|
+| Musiques actuelles| 01 |
+| Cinéma et audiovisuel | 02 |
+| Musiques classiques | 03 |
+| Cirque et Arts de la rue | 04 |
+| Pluridisciplinaire Spectacle vivant | 05 |
+| Livre et littérature | 06 |
+| Transdisciplinaire | 07 |
+| Divers Spectacle vivant | 08 |
+| Pluridisciplinaire Musique | 09 |
+| Danse | 10 |
+| Arts plastiques et visuels | 11 |
+| Théâtre | 12 |
+| Domaines divers | 13 |
+
+III. Get festival by department and domaine
+
+```
+getFestival(x, y)
+```
+This function search in the API for the festival with the domaine and the department.
+
+x = department number (exemple : 01, 25 or 65)
+y = domaine number (refer to the table upper)
+
+If you don't want to use one of this params replace it by "0".
+For example :
+```
+getFestival("0","11");
+```
+This will search the festival everywhere with the domaine : "Arts plastiques et visuels".
--- main.js
+++ main.js
@@ -5,3 +5,109 @@ export default async function getFestivalByDepartment(department){
     const data = await response.json();
     return data.records;
 }
+
+export async function getFestivalByDomaine(domaine){
+    var nomDomaine = "";
+    switch(domaine){
+        case '01':
+            nomDomaine = "Musiques actuelles";
+            break;
+        case '02':
+            nomDomaine = "Cinéma et audiovisuel";
+            break;
+        case '03':
+            nomDomaine = "Musiques classiques";
+            break;
+        case '04':
+            nomDomaine = "Cirque et Arts de la rue";
+            break;
+        case '05':
+            nomDomaine = "Pluridisciplinaire Spectacle vivant";
+            break;
+        case '06':
+            nomDomaine = "Livre et littérature";
+            break;
+        case '07':
+            nomDomaine = "Transdisciplinaire";
+            break;
+        case '08':
+            nomDomaine = "Divers Spectacle vivant";
+            break;
+        case '09':
+            nomDomaine = "Pluridisciplinaire Musique";
+            break;
+        case '10':
+            nomDomaine = "Danse";
+            break;
+        case '11':
+            nomDomaine = "Arts plastiques et visuels";
+            break;
+        case '12':
+            nomDomaine = "Théâtre";
+            break;
+        case '13':
+            nomDomaine = "Domaines divers";
+            break;
+    }
+
+    const response = await fetch("https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&refine.domaine=" + nomDomaine);
+    const data = await response.json();
+    return data.records;
+}
+
+export async function getFestival(departement, domaine){
+    var params = ""
+    if(departement != 0){
+        params += "&refine.departement=" + departement;
+    }
+    if(domaine != 0){
+        var nomDomaine = "";
+        switch(domaine){
+            case '01':
+                nomDomaine = "Musiques actuelles";
+                break;
+            case '02':
+                nomDomaine = "Cinéma et audiovisuel";
+                break;
+            case '03':
+                nomDomaine = "Musiques classiques";
+                break;
+            case '04':
+                nomDomaine = "Cirque et Arts de la rue";
+                break;
+            case '05':
+                nomDomaine = "Pluridisciplinaire Spectacle vivant";
+                break;
+            case '06':
+                nomDomaine = "Livre et littérature";
+                break;
+            case '07':
+                nomDomaine = "Transdisciplinaire";
+                break;
+            case '08':
+                nomDomaine = "Divers Spectacle vivant";
+                break;
+            case '09':
+                nomDomaine = "Pluridisciplinaire Musique";
+                break;
+            case '10':
+                nomDomaine = "Danse";
+                break;
+            case '11':
+                nomDomaine = "Arts plastiques et visuels";
+                break;
+            case '12':
+                nomDomaine = "Théâtre";
+                break;
+            case '13':
+                nomDomaine = "Domaines divers";
+                break;
+        }
+
+        params += "&refine.domaine=" + nomDomaine;
+    }
+
+    const response = await fetch("https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals" + params);
+    const data = await response.json();
+    return data.records;
+}
