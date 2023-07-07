c8924295bff9703101494d9682d32f82c8b0001a
Tekrodz <kevinkmmichel@gmail.com>
2023-07-07 20:05:58 +0200

    fix three

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let fetch = await import('node-fetch')
+import fetch from 'node-fetch';
 
 export default async function getFestivalByDepartment(department){
     const response = await fetch("https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&refine.departement=" + department);
